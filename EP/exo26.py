2c938abdb76d38b8f33655a7f823d7f5cbef9b35
tutur <arth.gauffre@gmail.com>
2022-01-25 09:41:50 +0100

    NSI

---
+++ EP/exo26.py
@@ -0,0 +1,21 @@
+# exo 26.1 :
+
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o,','p','q','r','s','t','u','v','w','x','y','z']
+
+def occurrence_max(chaine):
+    occurence = [0] *  26
+    for i in range(26):
+        compteur = 0
+        for caractere in chaine:
+            if caractere == alphabet[i]:
+                compteur += 1
+        occurence[i] = compteur
+    ind_max = 0
+    for i in range(26):
+        if occurence[i] > occurence[ind_max]:
+            ind_max = i
+    return alphabet[ind_max]
+
+ch = 'je suis en terminale et je passe le bac et je souhaite poursuivre des etudes pour devenir expert en informatique'
+print(occurrence_max(ch))
--- arbre/exo.py
+++ arbre/exo.py
@@ -1,6 +1,94 @@
-class Arbre():
+class Arbre:
     def __init__(self, data):
-         self.data = data
-         self.right = None
-         self.left = None
-         
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def set_left(self, sousarbre): # mutateur
+        self.left = sousarbre
+
+    def set_right(self, sousarbre): # mutateur
+        self.right = sousarbre  
+
+    def get_left(self): # accesseur
+        return self.left
+
+    def get_right(self): # accesseur
+        return self.right
+
+    def get_data(self): # accesseur
+        return self.data
+
+def prefixe(arbre):
+    if arbre is None :
+        return None
+    print(arbre.data, end = '-')
+    prefixe(arbre.left)
+    prefixe(arbre.right)
+
+def infixe(arbre):
+    if arbre is None :
+        return None
+    infixe(arbre.left)
+    print(arbre.data, end = '-')
+    infixe(arbre.right)
+
+def postfixe(arbre):
+    if arbre is None :
+        return None
+    postfixe(arbre.left)
+    postfixe(arbre.right)
+    print(arbre.data, end = '-')
+
+def taille(arbre):
+    if arbre is None:
+        return 0
+    else:
+        return 1 + taille(arbre.left) + taille(arbre.right)
+
+def hauteur(arbre):
+    if arbre is None:
+        return 0
+    else:
+        return 1 + max(hauteur(arbre.left), hauteur(arbre.right))
+
+def nbfeuilles(arbre):
+    if arbre is None:
+        return 0 
+    if arbre.left is None and arbre.right is None:
+        return 1
+    else:
+        return nbfeuilles(arbre.left) + nbfeuilles(arbre.right)
+
+def recherche(arbre, n):
+    if arbre is None:
+        return False
+    if arbre is n:
+        return True 
+    else:
+        return recherche(arbre.left, n) or recherche(arbre.right, n)
+
+# parcour infixe en interatif
+
+def interativeinorder(arbre):
+    s = []
+    while (s != []) or (arbre != None):
+        if arbre != None:
+            s.append(arbre)
+            arbre = arbre.left
+        else:
+            arbre = s.pop()
+            print(arbre.data)
+            arbre = arbre.right
+
+
+
+a = Arbre(9)
+a.left = Arbre(8)
+a.right = Arbre(7)
+a.left.left = Arbre(6)
+a.left.right = Arbre(2)
+print(taille(a))
+print(recherche(a, 2))
+print(recherche(a, 12))
+print(interativeinorder(a), infixe(a))

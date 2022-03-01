ee56b307670680a69d836a0bfe1a8465378b95b5
tutur <arth.gauffre@gmail.com>
2022-03-01 09:20:17 +0100

    NSI

--- EP/EP_officielle/sujet22.py
+++ EP/EP_officielle/sujet22.py
@@ -1,3 +1,13 @@
+#exo 1
+
+def renverse(mot):
+    mot_renverse = ''
+    for lettre in mot:
+        mot_renverse = lettre + mot_renverse
+    return mot_renverse
+        
+print(renverse("informatique"))
+
 #exo 2
 
 def crible(N):
@@ -7,12 +17,12 @@ def crible(N):
     premiers = []
     tab = [True] * N
     tab[0], tab[1] = False, False
-    for i in range(..., N):
-            if tab[i] == ...:
-                premiers.append(...)
-                for multiple in range(2*i, N, ...):
-                    tab[multiple] = ...
+    for i in range(2, N):
+            if tab[i] == True:
+                premiers.append(i)
+                for multiple in range(2*i, N, i):
+                    tab[multiple] = False
     return premiers
 
-
 assert crible(40) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+print(crible(40))
---
+++ diviserpourregner/interclassement.py
@@ -0,0 +1,14 @@
+def interclassement(lst1, lst2):
+    lst_totale = []
+    i1 = 0 
+    i2 = 0
+    while i1 != len(lst1) and i2 != len(lst2):
+        if lst1[i1] < lst2[i2]:
+            lst_totale.append(lst1[i1])
+            i1 += 1
+        else:
+            lst_totale.append(lst2[i2])
+            i2 += 1
+    return lst_totale + lst2[i2:] + lst1[i1:]
+
+print(interclassement([2,5,7,12], [1,4,7,8,9]))
